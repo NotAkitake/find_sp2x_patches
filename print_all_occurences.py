a8af8c5e08ea8e28da4f42f629c14ebe636db6ec
Akitake <akitake@pm.me>
2025-03-26 09:33:36 +0100

    Allow testing signatures against multiple dlls by recursively searching for them

--- print_all_occurences.py
+++ print_all_occurences.py
@@ -1,19 +1,22 @@
 from find_sp2x_patches import find
+from pathlib import Path
 
 if __name__ == "__main__":
-    # DLL to search inside of
-    dll_path = "./dlls/file.dll"
+    # Path to recursively find dll files under
+    dll_path = "./dlls/"
     # Signature to find all occurrences for
     signature = ""
     # Offset to start searching at
-    offset = 0
+    start_offset = 0
     # Adjustement to add to the output offset
     adjust = 0
 
-    print(dll_path)
-    with open(dll_path, 'r+b') as dll:
-        while True:
-            offset = find(signature, dll, offset+1, adjust)
-            if offset is None:
-                break
-            print(offset)
+    for dll_path in Path(dll_path).rglob("*.dll"):
+        offset = start_offset
+        print(dll_path)
+        with open(dll_path, 'r+b') as dll:
+            while True:
+                offset = find(signature, dll, offset+1, adjust)
+                if offset is None:
+                    break
+                print(offset)
